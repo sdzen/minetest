b886ad1aa4319d69622a3e429a7acf35f2b1f70c
Perttu Ahola <celeron55@gmail.com>
2012-12-01 17:17:01 +0200

    Do final brightness setting in linear space with exponential exposure

--- client/shaders/test_shader_1/opengl_fragment.glsl
+++ client/shaders/test_shader_1/opengl_fragment.glsl
@@ -6,11 +6,16 @@ varying vec3 vPosition;
 
 void main (void)
 {
+    //vec4 col = vec4(1.0, 0.0, 0.0, 1.0);
     vec4 col = texture2D(myTexture, vec2(gl_TexCoord[0]));
 	float a = col.a;
     col *= gl_Color;
-	col *= 1.5;
-    //vec4 col = vec4(1.0, 0.0, 0.0, 1.0);
+	col = col * col; // SRGB -> Linear
+	col *= 1.8;
+	col.r = 1.0 - exp(1.0 - col.r) / exp(1.0);
+	col.g = 1.0 - exp(1.0 - col.g) / exp(1.0);
+	col.b = 1.0 - exp(1.0 - col.b) / exp(1.0);
+	col = sqrt(col); // Linear -> SRGB
 	float d = max(0.0, min(vPosition.z / 800.0 - 0.8, 1.0));
 	col = mix(col, skyBgColor, d);
     gl_FragColor = vec4(col.r, col.g, col.b, a);
--- client/shaders/test_shader_2/opengl_fragment.glsl
+++ client/shaders/test_shader_2/opengl_fragment.glsl
@@ -8,7 +8,12 @@ void main (void)
     vec4 col = texture2D(myTexture, vec2(gl_TexCoord[0]));
     col *= gl_Color;
 	float a = gl_Color.a;
-	col *= 1.5;
+	col = col * col; // SRGB -> Linear
+	col *= 1.8;
+	col.r = 1.0 - exp(1.0 - col.r) / exp(1.0);
+	col.g = 1.0 - exp(1.0 - col.g) / exp(1.0);
+	col.b = 1.0 - exp(1.0 - col.b) / exp(1.0);
+	col = sqrt(col); // Linear -> SRGB
 	float d = max(0.0, min(vPosition.z / 800.0 - 0.8, 1.0));
 	a = mix(a, 0.0, d);
     gl_FragColor = vec4(col.r, col.g, col.b, a);
